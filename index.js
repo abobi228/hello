f86bee1ab2a37f6c8570ebec800d783722bf5028
John Doe <johndoe@example.com>
2023-10-15 02:43:16 +0300

    email proxy console.log

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const fs = require('fs');
+function parseEmails(filePath) {
+  const lines = fs.readFileSync(filePath, 'utf8').split('\n');
+  const emails= [];
+
+  lines.forEach((line) => {
+    const [email = '', imapPass = ''] = line.split(':');
+    emails.push({ email: email.trim(), imapPass: imapPass.trim() });
+  });
+
+  return emails;
+}
+function parseProxies(filePath) {
+  const lines = fs.readFileSync(filePath, 'utf8').split('\n');
+  const proxies = [];
+
+  lines.forEach((line) => {
+    const proxy = line.trim();
+    proxies.push(proxy);
+  });
+
+  return proxies;
+}
+const emails = parseEmails('./inputs/emails.txt');
+const proxies = parseProxies('./inputs/proxies.txt');
 
 
-const args = process.argv.slice(2);
-
-console.log('Received arguments:', args);
+console.log(emails,proxies)
